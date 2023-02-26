a451fb4990bda4672e141135f6b68adc8ade7ecf
Elizeu Walker Brizolla <elizeuwalkerbrizolla@hotmail.com>
2023-02-26 00:12:26 -0300

    implementado a classe Pagination

--- Backupasdasd.js
+++ Backupasdasd.js
@@ -1,16 +1,16 @@
+//import Pagination from "./assets/js/PaginationClass"
+
 /*
     O componente axios está sendo chamado no html:
     <script src="./node_modules/axios/dist/axios.min.js"></script>
 */
-import Pagination from './assets/js/Pagination.js'
-
 const url = 'https://rickandmortyapi.com/api/character'
 let pagAtual
 
 async function getCharacters(url, targetPage) {
     try {
         const response = await axios.get(url, { params: { page: targetPage } })         //Vai fazer uma req get com um parâmetro e retornar um json. Com axios não precisa fazer o parse
-        return response.data.results                                                    //Se não der jabu, será retornado uma promise com o array de objetos com os personagens
+        return response.data.results                                            //Se não der jabu, será retornado uma promise com o array de objetos com os personagens
     }
     catch (e) {
         console.error('Deu Jabu')
@@ -22,11 +22,11 @@ function selectData(arr) {
 }
 
 const paginationConteiner = document.getElementById('pagination-1')
-const cardPagination = new Pagination(42, paginationConteiner)
+//const pagination = new Pagination(42, paginationConteiner)
 
 paginationConteiner.addEventListener('click', (event) => {
     event.preventDefault()
-    pagAtual = cardPagination.onChangePage(pagAtual, event)
+    pagAtual = getPage(pagAtual, 42, paginationConteiner, event, false)
 
     getCharacters(url, pagAtual)
         .then(selectData)
@@ -35,30 +35,10 @@ paginationConteiner.addEventListener('click', (event) => {
 
 function onLoadEvent(event) {
     pagAtual = 1
+    getPage(pagAtual, 42, paginationConteiner, event, true)
 
     getCharacters(url, pagAtual)
         .then(selectData)
         .then(characterList => renderCard(characterList))
 }
 
-const cardConteiner = document.getElementById('character-card')
-
-function renderCard(characterList){
-    cardConteiner.innerHTML = ''
-
-    characterList.map(({ name, image }) => {
-        const div = document.createElement('div')
-        div.classList.add('flex-item')
-    
-        const img = document.createElement('img')
-        img.src = image
-    
-        const p = document.createElement('p')
-        p.innerText = name
-    
-        div.appendChild(img)
-        div.appendChild(p)
-    
-        cardConteiner.appendChild(div)
-    })
-}
